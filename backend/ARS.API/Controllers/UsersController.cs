eba34e6a1931bf6060890310edab718a7bbe2d4e
Efrain Dominguez <efrainbss@gmail.com>
2026-04-09 14:25:12 -0700

    Remove test endpoints and finalize API for production

--- backend/ARS.API/Controllers/UsersController.cs
+++ backend/ARS.API/Controllers/UsersController.cs
@@ -102,27 +102,5 @@ namespace ARS.API.Controllers
             return CreatedAtAction(nameof(GetUserById), new { id = createdUser.Id }, createdUser);
         }
 
-
-        #region temp methods for testing purposes, to be removed later
-
-        [HttpGet("test-error")]
-        public ActionResult TestError()
-        {
-            throw new Exception("This is a test exception!");
-        }
-
-        [HttpGet("test-unauthorized")]
-        public ActionResult TestUnauthorized()
-        {
-            throw new UnauthorizedAccessException("You don't have permission!");
-        }
-
-        [HttpGet("test-notfound")]
-        public ActionResult TestNotFound()
-        {
-            throw new KeyNotFoundException("User not found!");
-        }
-
-        #endregion
     }
 }
